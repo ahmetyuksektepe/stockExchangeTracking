76e43eb58bd3a32edf64901c66e0129fb4749125
Ahmet <112433462+ahmetyuksektepe@users.noreply.github.com>
2023-11-05 12:32:45 +0300

    Update vericekme.py

--- vericekme.py
+++ vericekme.py
@@ -1,32 +1,54 @@
 from bs4 import BeautifulSoup
-import requests
-
-url = "https://borsa.doviz.com/hisseler"
-result = requests.get(url)
-doc = BeautifulSoup(result.text, "html.parser")
-
-tbody = doc.tbody
-trs = tbody.find_all("tr")
+import requests,pandas,xlsxwriter
 
 
-for tr in trs:
-    td_list = tr.find_all("td")[1:7]
-    if len(td_list) == 6:
-        
-        anlik, yuksek, dusuk, hacim, degisim, saat = td_list
-        #Hisse Adını Hatalı Çektiği İçin değiştirdim
-        # hisse_adi = anlik.find_next(class_="currency-details").text.strip()
-        # hisse_adi = hisse_adi.split("\n")[0]
-        # sirket_adi = anlik.find_next(class_="cname").text.strip()
-        hisse_adi = tr['data-name']
 
-        print("Hisse - Şirket Adı:", hisse_adi)
-        print("Anlik fiyat:", anlik.text.strip())
-        print("Yuksek:", yuksek.text.strip())
-        print("Dusuk:", dusuk.text.strip())
-        print("Hacim:", hacim.text.strip())
-        print("Degisim:", degisim.text.strip())
-        print("Saat:", saat.text.strip())
-        print()
-      
-        
+URL = "https://borsa.doviz.com/hisseler"
+RASYO_URL_TEMPLATE = "https://www.isyatirim.com.tr/tr-tr/analiz/hisse/Sayfalar/sirket-karti.aspx?hisse="
+
+
+#Hissenin temel bilgilerini getirir
+def hisse_temel():
+    data = []
+    result = requests.get(URL)
+    doc = BeautifulSoup(result.text, "html.parser")
+    tbody = doc.tbody
+    trs = tbody.find_all("tr")
+    for tr in trs:
+        td_list = tr.find_all("td")[1:7]
+        if len(td_list) == 6:
+            anlik, yuksek, dusuk, hacim, degisim, saat = td_list
+            hisse_adi = tr['data-name'].split('-')
+            hisse = {
+                "Hisse Adi":hisse_adi[1],
+                "Hisse Kodu":hisse_adi[0],
+                "Anlik fiyat":anlik.text.strip(),
+                "Yuksek": yuksek.text.strip(),
+                "Dusuk":dusuk.text.strip(),
+                "Hacim":hacim.text.strip(),
+                "Degisim:":degisim.text.strip(),
+                "Saat":saat.text.strip(),
+                # "F/K":rasyo_list[0],
+                # "FD/FAVÖK":rasyo_list[1],
+                # "PD/DD":rasyo_list[2],
+                # "FD/Satışlar":rasyo_list[3],
+                # "Yabancı Oranı (%)":rasyo_list[4],
+                # "Ort Hacim (mn$) 3A/12A":rasyo_list[5],
+                # "Piyasa Değeri":rasyo_list[6],
+                # "Net Borç":rasyo_list[7],
+                # "Yurtdışı Çarpan İskontosu (%)":rasyo_list[8],
+                # "Halka Açıklık Oranı (%)":rasyo_list[9],
+            }
+            
+            data.append(hisse)
+    return data            
+
+#Data'yı excele yazdırır
+def excel_yazdir(data:list):
+    df = pandas.DataFrame(data)
+    writer = pandas.ExcelWriter("datas.xlsx",engine="xlsxwriter")
+    df.to_excel(writer,sheet_name="Sheet1",index=False)
+    writer.close()
+
+def rasyo_degerleri(data:list):
+    pass
